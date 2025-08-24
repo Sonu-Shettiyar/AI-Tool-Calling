85277b319009dbd9f46097a2946e3bbfdb61dcca
Sonu-Shettiyar <sonushettiyar9393@gmail.com>
2025-08-24 15:20:05 +0530

    using URLSearchPrams to trigger unauthenticated toast

--- src/components/showLoginToast.jsx
+++ src/components/showLoginToast.jsx
@@ -1,21 +1,19 @@
 "use client"
 
 import React, { useEffect } from "react"
-import { useSearchParams } from "next/navigation"
 import toast from "react-hot-toast"
 
 function ShowLoginToast() {
-    const searchParams = useSearchParams()
-    const unauthenticated = searchParams.get("unauthenticated")
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const unauthenticated = params.get("unauthenticated")
 
-    useEffect(() => {
-        if (unauthenticated === "true") {
-            toast.error("Unauthorized, Please login to continue")
-        };
-        return () => { }
-    }, [unauthenticated])
+    if (unauthenticated === "true") {
+      toast.error("Unauthorized, Please login to continue")
+    }
+  }, [])
 
-    return null
+  return null
 }
 
-export default React.memo(ShowLoginToast);
+export default React.memo(ShowLoginToast)
